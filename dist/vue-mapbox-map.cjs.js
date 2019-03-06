e1707e1cd88eef04deb37ccea4a6d363dca4dd6c
gareth <garethsimons@me.com>
2019-03-06 14:52:06 +0000

    version bumps

--- dist/vue-mapbox-map.cjs.js
+++ dist/vue-mapbox-map.cjs.js
@@ -163,9 +163,94 @@ var script = {
   }
 };
 
+function normalizeComponent(template, style, script, scopeId, isFunctionalTemplate, moduleIdentifier
+/* server only */
+, shadowMode, createInjector, createInjectorSSR, createInjectorShadow) {
+  if (typeof shadowMode !== 'boolean') {
+    createInjectorSSR = createInjector;
+    createInjector = shadowMode;
+    shadowMode = false;
+  } // Vue.extend constructor export interop.
+
+
+  var options = typeof script === 'function' ? script.options : script; // render functions
+
+  if (template && template.render) {
+    options.render = template.render;
+    options.staticRenderFns = template.staticRenderFns;
+    options._compiled = true; // functional template
+
+    if (isFunctionalTemplate) {
+      options.functional = true;
+    }
+  } // scopedId
+
+
+  if (scopeId) {
+    options._scopeId = scopeId;
+  }
+
+  var hook;
+
+  if (moduleIdentifier) {
+    // server build
+    hook = function hook(context) {
+      // 2.3 injection
+      context = context || // cached call
+      this.$vnode && this.$vnode.ssrContext || // stateful
+      this.parent && this.parent.$vnode && this.parent.$vnode.ssrContext; // functional
+      // 2.2 with runInNewContext: true
+
+      if (!context && typeof __VUE_SSR_CONTEXT__ !== 'undefined') {
+        context = __VUE_SSR_CONTEXT__;
+      } // inject component styles
+
+
+      if (style) {
+        style.call(this, createInjectorSSR(context));
+      } // register component module identifier for async chunk inference
+
+
+      if (context && context._registeredComponents) {
+        context._registeredComponents.add(moduleIdentifier);
+      }
+    }; // used by ssr in case component is cached and beforeCreate
+    // never gets called
+
+
+    options._ssrRegister = hook;
+  } else if (style) {
+    hook = shadowMode ? function () {
+      style.call(this, createInjectorShadow(this.$root.$options.shadowRoot));
+    } : function (context) {
+      style.call(this, createInjector(context));
+    };
+  }
+
+  if (hook) {
+    if (options.functional) {
+      // register for functional component in vue file
+      var originalRender = options.render;
+
+      options.render = function renderWithStyleInjection(h, context) {
+        hook.call(context);
+        return originalRender(h, context);
+      };
+    } else {
+      // inject component registration as beforeCreate hook
+      var existing = options.beforeCreate;
+      options.beforeCreate = existing ? [].concat(existing, hook) : [hook];
+    }
+  }
+
+  return script;
+}
+
+var normalizeComponent_1 = normalizeComponent;
+
 /* script */
-            var __vue_script__ = script;
-            
+var __vue_script__ = script;
+
 /* template */
 var __vue_render__ = function() {
   var _vm = this;
@@ -184,98 +269,20 @@ __vue_render__._withStripped = true;
   var __vue_module_identifier__ = undefined;
   /* functional template */
   var __vue_is_functional_template__ = false;
-  /* component normalizer */
-  function __vue_normalize__(
-    template, style, script$$1,
-    scope, functional, moduleIdentifier,
-    createInjector, createInjectorSSR
-  ) {
-    var component = (typeof script$$1 === 'function' ? script$$1.options : script$$1) || {};
-
-    // For security concerns, we use only base name in production mode.
-    component.__file = "/Users/gareth/dev/github/vue-mapbox-map/src/VueMapboxMap.vue";
-
-    if (!component.render) {
-      component.render = template.render;
-      component.staticRenderFns = template.staticRenderFns;
-      component._compiled = true;
-
-      if (functional) { component.functional = true; }
-    }
-
-    component._scopeId = scope;
-
-    return component
-  }
   /* style inject */
-  function __vue_create_injector__() {
-    var head = document.head || document.getElementsByTagName('head')[0];
-    var styles = __vue_create_injector__.styles || (__vue_create_injector__.styles = {});
-    var isOldIE =
-      typeof navigator !== 'undefined' &&
-      /msie [6-9]\\b/.test(navigator.userAgent.toLowerCase());
-
-    return function addStyle(id, css) {
-      if (document.querySelector('style[data-vue-ssr-id~="' + id + '"]')) { return } // SSR styles are present.
-
-      var group = isOldIE ? css.media || 'default' : id;
-      var style = styles[group] || (styles[group] = { ids: [], parts: [], element: undefined });
-
-      if (!style.ids.includes(id)) {
-        var code = css.source;
-        var index = style.ids.length;
-
-        style.ids.push(id);
-
-        if (isOldIE) {
-          style.element = style.element || document.querySelector('style[data-group=' + group + ']');
-        }
-
-        if (!style.element) {
-          var el = style.element = document.createElement('style');
-          el.type = 'text/css';
-
-          if (css.media) { el.setAttribute('media', css.media); }
-          if (isOldIE) {
-            el.setAttribute('data-group', group);
-            el.setAttribute('data-next-index', '0');
-          }
-
-          head.appendChild(el);
-        }
-
-        if (isOldIE) {
-          index = parseInt(style.element.getAttribute('data-next-index'));
-          style.element.setAttribute('data-next-index', index + 1);
-        }
-
-        if (style.element.styleSheet) {
-          style.parts.push(code);
-          style.element.styleSheet.cssText = style.parts
-            .filter(Boolean)
-            .join('\n');
-        } else {
-          var textNode = document.createTextNode(code);
-          var nodes = style.element.childNodes;
-          if (nodes[index]) { style.element.removeChild(nodes[index]); }
-          if (nodes.length) { style.element.insertBefore(textNode, nodes[index]); }
-          else { style.element.appendChild(textNode); }
-        }
-      }
-    }
-  }
+  
   /* style inject SSR */
   
 
   
-  var VueMapboxMap = __vue_normalize__(
+  var VueMapboxMap = normalizeComponent_1(
     { render: __vue_render__, staticRenderFns: __vue_staticRenderFns__ },
     __vue_inject_styles__,
     __vue_script__,
     __vue_scope_id__,
     __vue_is_functional_template__,
     __vue_module_identifier__,
-    __vue_create_injector__,
+    undefined,
     undefined
   );
 
--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,15 @@
+import resolve from 'rollup-plugin-node-resolve'
+import commonjs from 'rollup-plugin-commonjs'
 import vue from 'rollup-plugin-vue'
 import buble from 'rollup-plugin-buble'
 import pkg from './package.json' // import names from package to reduce errors
 
 // see https://github.com/rollup/rollup-starter-lib/blob/buble/rollup.config.js
-export default [
-  // browser friendly UMD build
-  {
-    input: 'src/index.js',
-    external: [ 'mapbox-gl', '@mapbox/mapbox-gl-geocoder' ],
-    output: {
+export default {
+  input: 'src/index.js',
+  external: [ 'mapbox-gl', '@mapbox/mapbox-gl-geocoder' ], // suppresses warnings about external modules
+  output: [
+    {
       name: 'VueMapboxMap',
       file: pkg.browser,
       format: 'umd',
@@ -17,48 +18,33 @@ export default [
         '@mapbox/mapbox-gl-geocoder': 'MapboxGeocoder'
       }
     },
-    plugins: [
-      vue({
-        compileTemplate: true, // true by default
-        css: true
-      }),
-      buble({
-        exclude: ['node_modules/**']
-      })
-    ]
-  },
-  // CommonJS (for Node) and ES module (for bundlers) build
-  {
-    input: 'src/index.js',
-    external: [ 'mapbox-gl', '@mapbox/mapbox-gl-geocoder' ], // suppresses warnings about external modules
-    output: [
-      {
-        name: 'VueMapboxMap',
-        file: pkg.main,
-        format: 'cjs',
-        globals: {
-          'mapbox-gl': 'mapboxgl',
-          '@mapbox/mapbox-gl-geocoder': 'MapboxGeocoder'
-        }
-      },
-      {
-        name: 'VueMapboxMap',
-        file: pkg.module,
-        format: 'es',
-        globals: {
-          'mapbox-gl': 'mapboxgl',
-          '@mapbox/mapbox-gl-geocoder': 'MapboxGeocoder'
-        }
+    {
+      name: 'VueMapboxMap',
+      file: pkg.main,
+      format: 'cjs',
+      globals: {
+        'mapbox-gl': 'mapboxgl',
+        '@mapbox/mapbox-gl-geocoder': 'MapboxGeocoder'
       }
-    ],
-    plugins: [
-      vue({
-        compileTemplate: true, // true by default
-        css: true
-      }),
-      buble({
-        exclude: ['node_modules/**']
-      })
-    ]
-  }
-]
+    },
+    {
+      name: 'VueMapboxMap',
+      file: pkg.module,
+      format: 'es',
+      globals: {
+        'mapbox-gl': 'mapboxgl',
+        '@mapbox/mapbox-gl-geocoder': 'MapboxGeocoder'
+      }
+    }
+  ],
+  plugins: [
+    resolve(),
+    commonjs(),
+    vue({
+      css: true
+    }),
+    buble({
+      exclude: ['node_modules/**']
+    })
+  ]
+}
