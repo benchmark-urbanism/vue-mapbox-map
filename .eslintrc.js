3b3f0f1749bf6a9c51f78373264741142f4791ed
gareth <garethsimons@me.com>
2019-11-07 11:14:07 +0000

    changes eslint from standard to prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,15 +3,15 @@ module.exports = {
   env: {
     node: true
   },
-  'extends': [
-    'plugin:vue/recommended',
-    '@vue/standard'
+  extends: [
+    "plugin:vue/recommended",
+    "@vue/prettier"
   ],
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
   },
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: "babel-eslint"
   }
-}
+};
--- rollup.config.js
+++ rollup.config.js
@@ -1,36 +1,36 @@
-import resolve from 'rollup-plugin-node-resolve'
-import commonjs from 'rollup-plugin-commonjs'
-import vue from 'rollup-plugin-vue'
-import buble from 'rollup-plugin-buble'
-import { terser } from 'rollup-plugin-terser'
+import resolve from "rollup-plugin-node-resolve";
+import commonjs from "rollup-plugin-commonjs";
+import vue from "rollup-plugin-vue";
+import buble from "rollup-plugin-buble";
+import { terser } from "rollup-plugin-terser";
 
 // see https://github.com/rollup/rollup-starter-lib/blob/buble/rollup.config.js
 export default {
-  input: 'src/main.js',
-  external: [ 'mapbox-gl', '@mapbox/mapbox-gl-geocoder' ], // suppresses warnings about external modules
+  input: "src/main.js",
+  external: ["mapbox-gl", "@mapbox/mapbox-gl-geocoder"], // suppresses warnings about external modules
   output: [
     {
-      name: 'VueMapboxMap',
-      file: 'dist/VueMapboxMap.umd.js',
-      format: 'umd', // browser friendly UMD build
+      name: "VueMapboxMap",
+      file: "dist/VueMapboxMap.umd.js",
+      format: "umd", // browser friendly UMD build
       globals: {
-        'mapbox-gl': 'mapboxgl'
+        "mapbox-gl": "mapboxgl"
       }
     },
     {
-      name: 'VueMapboxMap',
-      file: 'dist/VueMapboxMap.common.js',
-      format: 'cjs', // CommonJS (for Node)
+      name: "VueMapboxMap",
+      file: "dist/VueMapboxMap.common.js",
+      format: "cjs", // CommonJS (for Node)
       globals: {
-        'mapbox-gl': 'mapboxgl'
+        "mapbox-gl": "mapboxgl"
       }
     },
     {
-      name: 'VueMapboxMap',
-      file: 'dist/VueMapboxMap.esm.js',
-      format: 'esm', // ES module (for bundlers)
+      name: "VueMapboxMap",
+      file: "dist/VueMapboxMap.esm.js",
+      format: "esm", // ES module (for bundlers)
       globals: {
-        'mapbox-gl': 'mapboxgl'
+        "mapbox-gl": "mapboxgl"
       }
     }
   ],
@@ -41,8 +41,8 @@ export default {
       css: true
     }),
     buble({
-      exclude: ['node_modules/**']
+      exclude: ["node_modules/**"]
     }),
     terser()
   ]
-}
+};
